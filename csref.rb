4281a8340f13f0167cb9eab9c485f8d493298fe3
Norio Kobayashi <noriokob@gmail.com>
2016-02-20 12:38:53 +0900

    update script

--- csref.rb
+++ csref.rb
@@ -1,24 +1,10 @@
 require 'nokogiri'
 
-# -----
-# $ csref
-# => クラス名一覧を表示
-#
-# $ csref クラス名
-# => クラス名のメソッド一覧を表示
-#
-# $ csref クラス名 メソッド名
-# => クラス名のメソッドのシグネチャとサンプルを表示
-#
-# クラス名、メソッド名は前方一致で検索
-# 複数の候補が存在する場合には、その候補の一覧を出力するのみ。
-# 完全マッチする項目が一つの場合は、それを出力する(List.Remove, List.RemoveAt での Remove)
-#
-
-# find_xxx    : 引数で指定した名前にマッチする対称を集める(大文字小文字区別しない)
-# collect_xxx : 対称を全て集める
-
 XML_FILENAME = 'cs.xml'
+
+def check(e)
+  raise unless e
+end
 
 def get_xml_path(xml_filename)
   dir = File.expand_path(File.dirname(__FILE__))
@@ -32,71 +18,110 @@ end
 def collect_class_names(doc)
   # class 一覧を返す
   ns = doc.xpath('//cs/class')
-  ns.map {|e| e.attribute('name').value }
+  ns.map {|e| e.attribute('name').value }.sort
 end
 
-def find_classes(doc, class_name)
+def collect_struct_names(doc)
+  ns = doc.xpath('//cs/struct')
+  ns.map {|e| e.attribute('name').value }.sort
+end
+
+def find_classes(doc, word)
   ns = doc.xpath('//cs/class')
 
   ret = []
   ns.each {|e|
     name = e.attribute('name').value.downcase
-    if name.start_with?(class_name.downcase)
-      # puts "FOUND: #{name}"
+    if name.start_with?(word.downcase)
+      # puts "found: #{name}"
       ret << e
     end
   }
   ret
 end
 
-#def find_interfaces(doc)
-#  # interface 一覧を返す
-#  ns = doc.xpath('//cs/interface')
-#  ns.map {|e| e.attribute('name').value }
-#end
+def find_structs(doc, word)
+  ns = doc.xpath('//cs/struct')
 
-def collect_tag(doc, class_name, tag_name)
-  doc.xpath("//cs/class[@name='#{class_name}']/#{tag_name}")
+  ret = []
+  ns.each {|e|
+    name = e.attribute('name').value.downcase
+    if name.start_with?(word.downcase)
+      # puts "found: #{name}"
+      ret << e
+    end
+  }
+  ret
 end
 
-def collect_properties(doc, class_name)
-  collect_tag(doc, class_name, 'property')
+# 文字列のリストを出力するときに、80 カラムで折り返すようにする
+# 文字列の途中では改行しない
+def display_list(slist)
+  ret = []
+  s = ''
+  slist.each {|e|
+    if (s + e).length >= 80
+      ret << s
+      s = ""
+    end
+    s += ' ' if s.length > 0
+    s += e
+  }
+
+  ret << s if s.length > 0
+  puts ret.join("\n")
 end
 
-def collect_methods(doc, class_name)
-  collect_tag(doc, class_name, 'method')
-end
+# API
+def csref_class_or_struct(doc, word)
+  classes = find_classes(doc, word) + find_structs(doc, word)
+  case classes.size
+  when 0
+    puts "NOT FOUND: #{word}"
+  when 1
+    # メソッド一覧を出力する
+    parent = classes[0]
 
-def collect_static_methods(doc, class_name)
-  collect_tag(doc, class_name, 'static-method')
-end
+    tagname = parent.name # class or struct
+    kname = parent.attribute('name').value
+    puts "#{tagname} #{kname}"
+    puts
 
-def disp_method_content(doc, class_name, method_name)
-  # puts "FIND: #{class_name} #{method_name}"
 
-  ns = doc.xpath("//cs/class[@name='#{class_name}']/method[@name='#{method_name}']")
-  if ns.size == 0
-    # static method を探す
-    ns = doc.xpath("//cs/class[@name='#{class_name}']/static-method[@name='#{method_name}']")
-  end
+    s_methods = parent.xpath('static-method')
+    if s_methods.size > 0
+      puts '--- static methods ---'
+      display_list(s_methods.map {|e| e.attribute('name').value }.sort)
+      puts
+    end
 
-  if ns.size == 0
-    # 見つからなかった
-    puts "見つかりませんでした class_name:#{class_name} method_name:#{method_name}"
-    return
-  elsif ns.size == 1
-    # マッチする項目が一つだけ見つかった
+    methods = parent.xpath('method')
+    if methods.size > 0
+      puts '--- methods ---'
+      display_list(methods.map {|e| e.attribute('name').value }.sort)
+      puts
+    end
+
+    properties = parent.xpath('property')
+    if properties.size > 0
+      puts '--- property ---'
+      display_list(properties.map {|e| e.attribute('name').value }.sort)
+      puts
+    end
   else
-    # 複数の候補が見つかった
-    puts "候補が複数見つかりました class_name:#{class_name} method_name:#{method_name}"
-    p ns
-    raise
+    # 複数の候補が見つかったので、クラス名を出力する
+    puts "#{class_name} にマッチする候補が複数みつかりました:"
+    puts classes.map {|e| e.attribute('name').value }.join(" ")
   end
+end
+
+def display_method(element, parent)
+  check(element.name == 'method' || element.name == 'static-method')
 
   sigs = []
   desc = ""
   example = ""
-  ns.children.each {|e|
+  element.children.each {|e|
     case e.name # タグ名
     when 'sig'
       # puts "sig=> #{e.text}"
@@ -126,7 +151,7 @@ def disp_method_content(doc, class_name, method_name)
     end
   }
 
-  puts "#{class_name}.#{method_name}"
+  puts "#{parent.attribute("name").value}.#{element.attribute("name").value}"
 
   puts
   puts "Signature:"
@@ -146,105 +171,60 @@ def disp_method_content(doc, class_name, method_name)
   end
 end
 
-# 文字列のリストを出力するときに、80 カラムで折り返すようにする
-# 文字列の途中では改行しない
-def format_list(slist)
-  ret = []
-  s = ''
-  slist.each {|e|
-    if (s + e).length >= 80
-      ret << s
-      s = ""
-    end
+def display_content(element, parent)
+  # puts "#{parent.name} #{parent.attribute("name").value}"
+  # puts "element:#{element.name} tag:#{element.attribute("name").value}"
 
-    s += ' ' if s.length > 0
-    s += e
-  }
+  case element.name
+  when "method"
+    # puts "method ->"
+    display_method(element, parent)
 
-  ret << s if s.length > 0
-  ret.join("\n")
-end
-
-# API
-def csref_class(doc, class_name)
-  classes = find_classes(doc, class_name)
-  case classes.size
-  when 0
-    puts "NOT FOUND: #{class_name}"
-  when 1
-    # メソッド一覧を出力する
-    kname = classes[0].attribute('name').value
-    puts "class #{kname}"
-    puts
-
-    s_methods = collect_static_methods(doc, kname)
-    if s_methods.size > 0
-      puts '--- static methods ---'
-      puts format_list(s_methods.map {|e| e.attribute('name').value }.sort)
-      puts
-    end
-
-    methods = collect_methods(doc, kname)
-    if methods.size > 0
-      puts '--- methods ---'
-      puts format_list(methods.map {|e| e.attribute('name').value }.sort)
-      puts
-    end
-
-    properties = collect_properties(doc, kname)
-    if properties.size > 0
-      puts '--- property ---'
-      puts format_list(properties.map {|e| e.attribute('name').value }.sort)
-      puts
-    end
+  when "static-method"
+    # puts "static method ->"
+    display_method(element, parent)
+  when "property"
+    # puts "property ->"
+    puts "プロパティは未実装"
   else
-    # 複数の候補が見つかったので、クラス名を出力する
-    puts "#{class_name} にマッチする候補が複数みつかりました:"
-    puts classes.map {|e| e.attribute('name').value }.join(" ")
+    fail
   end
 end
 
-def csref_method(doc, class_name, method_name)
-  classes = find_classes(doc, class_name)
-  if classes.size != 1
-    if classes.size == 0
-      puts "クラス名が見つかりません: #{class_name}"
-    else
-      puts "#{class_name} にマッチする候補が複数みつかりました:"
-      puts classes.map {|e| e.attribute('name').value }.join(" ")
-    end
+def csref_search(doc, word, subword)
+  ms = find_classes(doc, word) + find_structs(doc, word)
+  if ms.empty?
+    puts "クラス/構造体名が見つかりません: #{word}"
+    exit
+  elsif ms.size > 1
+    puts "#{word} にマッチする候補が複数みつかりました:"
+    puts classes.map {|e| e.attribute('name').value }.join(" ")
     exit
   end
 
-  # method_name にマッチするメソッド、静的メソッドを集める
-  methods = []
-
-  perfect_class_name = classes[0].attribute('name').value # 正式なクラス名
-  ['method', 'static-method'].each {|target|
-    classes[0].xpath(target).each {|e|
+  parent = ms[0]
+  candidates = []
+  perfect_class_name = parent.attribute('name').value # 正式なクラス名
+  ['method', 'static-method', 'property'].each {|target|
+    parent.xpath(target).each {|e|
       name = e.attribute('name').value
-      if name.downcase.start_with?(method_name.downcase)
+      if name.downcase.start_with?(subword.downcase)
         # puts "match method: #{perfect_class_name}.#{name}"
-        methods << e
+        candidates << e
       end
     }
   }
 
-  if methods.size == 0
-    puts "クラス #{perfect_class_name} のメソッドが見つかりません: #{method_name}"
-  elsif m = methods.find {|e| e.attribute('name').value.downcase == method_name.downcase }
-    # 完全マッチしている項目があるならそれを表示
-    # そうでなければ項目名のみ表示
-
-    # puts "perfect match #{method_name}"
-    perfect_method_name = m.attribute('name').value
-    disp_method_content(doc, perfect_class_name, perfect_method_name)
-  elsif methods.size == 1 # 候補が一つしかない
-    perfect_method_name = methods[0].attribute('name').value
-    disp_method_content(doc, perfect_class_name, perfect_method_name)
+  if candidates.empty?
+    puts "クラス #{perfect_class_name} のメソッドが見つかりません: #{subword}"
+  elsif m = candidates.find {|e| e.attribute('name').value.downcase == subword.downcase }
+    # 完全マッチする候補が存在する
+    display_content(m, parent)
+  elsif candidates.size == 1 # 候補が一つしかない
+    display_content(candidates[0], parent)
   else
-    puts "クラス #{perfect_class_name} に複数のメソッド候補が見つかりました:"
-    puts methods.map {|e| e.attribute('name').value }.join(" ")
+    puts "複数の候補が見つかりました:"
+    puts candidates.map {|e| e.attribute('name').value }.join(" ")
   end
 end
 
@@ -253,22 +233,20 @@ def main
 
   case ARGV.size
   when 0
-    # クラス名一覧を表示する
-    # class, interface, alias 一覧
-    puts format_list(collect_class_names(doc))
-
+    # クラス/構造体名一覧を表示する
+    xs = collect_class_names(doc) + collect_struct_names(doc)
+    display_list(xs.sort)
   when 1
-    # クラス名のメソッド一覧を表示する
-    class_name = ARGV[0]
-    csref_class(doc, class_name)
-
+    # クラス/構造体名のメソッド一覧を表示する
+    word = ARGV[0]
+    csref_class_or_struct(doc, word)
   when 2
     # クラス名.メソッドのシグネチャとサンプルを表示する
     # (class|interface|alias) (field|method|staticmethod)
-    class_name = ARGV[0]
-    method_name = ARGV[1]
+    word    = ARGV[0]
+    subword = ARGV[1]
 
-    csref_method(doc, class_name, method_name)
+    csref_search(doc, word, subword)
   else
     puts "USAGE"
     puts "$ csref"
